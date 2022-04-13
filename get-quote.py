41bd896fd431287b84135662e12fd48946259934
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-13 12:24:00 +0000

    Added file reading and writing

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,26 @@
+from distutils.file_util import write_file
 import random
 
 
 def primary():
     print("Keep it logically awesome.")
+    inputQuote = input("Add quote")
 
-    f = open("quotes.txt")
-    quotes = f.readlines()
+    f = open("quotes.txt", "a")
+    f.write(inputQuote)
+    f.write("\n")
     f.close()
 
-    last = len(quotes) - 1
-    rnd = random.randint(0, last)
+    f = open("quotes.txt", "r")
+    quotes = f.readlines()
+    f.close()
+    # print(quotes)
 
-    print(quotes[rnd])
+    # last = len(quotes) - 1
+    # rnd = random.randint(0, last)
+
+    for quote in quotes:
+        print(quote.rstrip())
 
 
 if __name__ == "__main__":
